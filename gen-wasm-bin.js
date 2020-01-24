fed5e52d87d2fce6c4f7d1725317ee1361cbdf34
usm-takl <usm.takl@google.com>
2020-01-25 08:33:34 +0900

    instantiate a header-only buffer source

--- gen-wasm-bin.js
+++ gen-wasm-bin.js
@@ -1,4 +1,4 @@
-const bufferSource = Uint8Array.of();
+const bufferSource = Uint8Array.of(0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00);
 const importObject = {};
 WebAssembly.instantiate(bufferSource, importObject).then(x => {
     console.log(x)
